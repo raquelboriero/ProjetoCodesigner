e2964bc0414d4e412666ee876e2f95c93a94a990
Raquel Boriero <raquelboriero@hotmail.com>
2021-05-21 17:15:40 +0100

    .

--- html+css+js/js/main.js
+++ html+css+js/js/main.js
@@ -1,5 +1,18 @@
+//Declarando variáveis
 var btnContact = document.querySelector('.jl-btn-contact');
 
+//page preloader
+window.addEventListener('load', function(){
+    var pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('jl-fade-out');
+     
+    /*setTimeout()
+    Executa um bloco específico uma vez depois de um determinado tempo 5000=5"*/
+    
+});
+
+
+// ícone de Contato
 // evento para quando clicar executar a função
 btnContact.addEventListener('click', function () {
     var boxContact = document.querySelector('.jl-contact-info');
