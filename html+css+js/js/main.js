483b402c7fe181e42abf3b746d4905b24238908e
Raquel Boriero <raquelboriero@hotmail.com>
2021-05-25 17:15:27 +0100

    .

--- html+css+js/js/main.js
+++ html+css+js/js/main.js
@@ -5,9 +5,14 @@ var btnContact = document.querySelector('.jl-btn-contact');
 window.addEventListener('load', function(){
     var pagePreloader = document.querySelector('.jl-preloader');
     pagePreloader.classList.add('jl-fade-out');
+
+    this.setTimeout(function () {
+        pagePreloader.style.display = 'none';
+    }, 2000)
+    /*foi necessário acresecentar o display none para que o Preloader saísse de vez e a função do botão voltasse a funcionar*/
      
     /*setTimeout()
-    Executa um bloco específico uma vez depois de um determinado tempo 5000=5"*/
+    Executa um bloco específico uma vez depois de um determinado tempo 2000=2"*/
     
 });
 
